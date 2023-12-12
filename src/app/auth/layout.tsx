5fac56b21435c3eb7918bafd88ad346489979e96
Fatima <Wayo@pces.mk>
2023-12-12 16:02:17 +0000

    auth screens initial

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import Image from 'next/image'
+import authCartoon from '../../../public/assets/auth-vector.png'
 
 interface Props {
-    children : React.ReactNode
+  children: React.ReactNode
 }
 
-export default function AuthLayout({children}: Props) {
+export default function AuthLayout({ children }: Props) {
   return (
-    <section>
-      <div>{children}</div>
-    </section>
+    <main className='flex h-screen'>
+      <section className='flex-1 items-center justify-center bg-powderblue'>
+        <Image className='flex justify-center' src={authCartoon} alt="money-art" />
+      </section>
+      <section className='flex-1 bg-babypowder'>
+        {children}
+      </section>
+    </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+const config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      slategray: '#738290',
+      powderblue: '#A1B5D8',
+      babypowder: '#FFFCF7',
+      nyanza: '#E4F0D0',
+      teagreen: '#C2D8B9'
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'authbackground': "url('/assets/background.jpg')"  
       },
     },
   },
