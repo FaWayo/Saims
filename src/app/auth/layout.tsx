9fa993f7b50feb7e8b2283c94dea889503853dbf
Fatima <Wayo@pces.mk>
2023-12-13 12:52:40 +0000

    auth layout update

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import Image from 'next/image'
-import authCartoon from '../../../public/assets/auth-vector.png'
-
+import React from "react";
+import authCartoon from "../../../public/assets/cartoononlayout.png";
+import authCartoon2 from "../../../public/assets/cartoon.png";
+import authCartoon3 from "../../../public/assets/cartoononlaay.png"
+import Carousel from "@/components/carousel/Carousel";
 interface Props {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
+const imageItems = [authCartoon2, authCartoon, authCartoon3];
+
 export default function AuthLayout({ children }: Props) {
+
   return (
-    <main className='flex h-screen'>
-      <section className='flex-1 items-center justify-center bg-powderblue'>
-        <Image className='flex justify-center' src={authCartoon} alt="money-art" />
+    <main className="flex h-screen">
+      <section className="flex-1 bg-delftblue">
+        <figure className="flex items-center justify-center h-full">
+          <Carousel
+            items={imageItems}
+          />
+        </figure>
       </section>
-      <section className='flex-1 bg-babypowder'>
-        {children}
-      </section>
+      <section className="flex-1 bg-aliceblue">{children}</section>
     </main>
-  )
+  );
 }
