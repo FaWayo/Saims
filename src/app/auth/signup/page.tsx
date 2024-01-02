df57ace071af2e5f0d5f7d39e96eae53a0e65723
Fatima <Wayo@pces.mk>
2024-01-02 06:50:34 +0000

    still on auth screens :(

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React, { useState } from "react";
 import Input from "@/components/Input";
-
+import Button from "@/components/Button";
+import { FcSalesPerformance } from "react-icons/fc";
 //create components for buttons and inputs
 
 function SignUp() {
@@ -11,23 +12,51 @@ function SignUp() {
   const [password2, setPassword2] = useState("");
 
   return (
-    <div className="flex flex-col justify-center items-center pt-40 pl-20">
-      <h1 className="font-bold font-header">Create Account</h1>
-      <p>Create an account for your salesperson</p>
+    <main className="flex h-screen">
+      <section className="w-2/4 bg-powderblue flex flex-col justify-between pl-5 text-white">
+        <div className="flex justify-start items-center mt-5">
+          <FcSalesPerformance className="h-10 w-10 border-2 border-yellow-100" />
+          <span className="font-bold font-header text-2xl pl-2 font-outline-2">
+            RAFFBY
+          </span>
+        </div>
 
-      <form>
-        <Input label="Full Name" id="fullname" value={fullName} setValue={setFullName} />
-        <Input label="Phone Number"  id="phoneNumber" value={phoneNumber} setValue={setPhoneNumber} />
-        <Input label="Password" id="password1"  value={password1} setValue={setPassword1} />
-        <Input label="Password" id="password2" value={password2} setValue={setPassword2} />
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="font-bold text-5xl">Welcome Back!</h1>
+          <p className="mt-5">
+            Please login with your personal info to streamline your sales and
+            optimize your inventory
+          </p>
 
-        <button>
-         {/* {label} */}
-          Sign Up
-        </button>
-      </form>
-    </div>
+          <Button label="SIGN IN" />
+        </div>
+
+        <div></div>
+
+      </section>
+
+      <section className="w-3/4">
+        {/* Content for the second section */}
+      </section>
+    </main>
   );
 }
 
 export default SignUp;
+
+// <div className="flex flex-col justify-left pt-40 pl-20">
+//   <p className="font-bold text-2xl">Create Account</p>
+//   <p className="font-light pt-2 text-lg">Create an account for your salesperson</p>
+
+//   <form className="pt-5">
+//     <Input label="Full Name" id="fullname" value={fullName} setValue={setFullName} />
+//     <Input label="Phone Number"  id="phoneNumber" value={phoneNumber} setValue={setPhoneNumber} />
+//     <Input label="Password" id="password1"  value={password1} setValue={setPassword1} />
+//     <Input label="Password" id="password2" value={password2} setValue={setPassword2} />
+
+//     <button>
+//      {/* {label} */}
+//       Sign Up
+//     </button>
+//   </form>
+// </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,7 @@
+
+import colors from 'tailwindcss/colors'
 const config = {
+
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -9,26 +12,33 @@ const config = {
       slategray: '#738290',
       powderblue: '#A1B5D8',
       babypowder: '#FFFCF7',
+      charcoal: '#313E50',
       delftblue: '#23395B',
       nyanza: '#E4F0D0',
       teagreen: '#C2D8B9',
       magnolia: '#ECE8EF',
-      aliceblue: '#E7F0FF'
-    },
-    fontFamily: {
-      // "header": "var(--header-font)",
-      // "body": "var(--body-font)",
-      // "body2": "var(--body2-font)"
-      "header": ['Montserrat'],
-      "body": ['Lato'],
-      "body2": ['Raleway']
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      aliceblue: '#E7F0FF',
+      licroice: '#1F1300'
     },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'authbackground': "url('/assets/background.jpg')"  
+        'authbackground': "url('/assets/backgrounda.jpg')"  
+      },
+      fontFamily: {
+        "header": ['var(--font-montserrat)'],
+        "body": ['var(--font-lato)'],
+        "body2": ['var(--font-raleway)']
       },
     },
   },
