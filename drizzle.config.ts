f2c5760234e96829a09b90aad203d06bce7045f6
Fatima <wayofatima@gmail.com>
2025-08-19 16:43:42 +0000

    env update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     dialect: 'postgresql',
     schema: './db/schema',
     dbCredentials: {
-        url: "postgresql://SaimsRaffby_owner:w1ycRt5TrICW@ep-falling-waterfall-a5qn6id0-pooler.us-east-2.aws.neon.tech/SaimsRaffby?sslmode=require&channel_binding=require"
+        url: `${process.env.DATABASE_URL}`
     }
 })
 
